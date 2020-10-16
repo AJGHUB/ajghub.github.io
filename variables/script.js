bc5d4dc0dbf378b8b733796b48302ad53c3b3be3
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-16 20:01:23 +1300

    add variables demo

--- variables/script.js
+++ variables/script.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 var updateHandler = function () {
     const suffix = this.dataset.sizing || ''
-document.documentElement.style.setProperty(`----${this.name}`, this.value + suffix)
+document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
 inputs.forEach(input => {
