f1c0182ee96bac204cb12e13e63e57aeac515945
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-23 00:36:57 +1300

    style.css

---
+++ flex-panel-gallery/script.js
@@ -0,0 +1,28 @@
+const panels = document.querySelectorAll('.panel')
+
+var toggleOpen = function () {
+    if (!this.classList.contains('open')) {
+        panels.forEach(panel => {
+            panel.classList.remove('open')
+            panel.classList.remove('open-active')
+        })
+        this.classList.toggle('open')
+    } else {
+        panels.forEach(panel => {
+            panel.classList.remove('open')
+            panel.classList.remove('open-active')
+        })
+    }
+}
+
+var toggleActive = function (e) {
+    if (e.propertyName.includes('flex')) {
+        if(this.classList.contains('open')){
+            this.classList.toggle('open-active')
+        }
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+
+panels.forEach(panel.addEventListener('transitioned', toggleActive))
