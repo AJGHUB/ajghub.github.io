9585c6d5d94f11331a036e1af69f281b4abca976
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-26 15:02:35 +1300

    js function css flex panels

--- flex-panel-gallery/script.js
+++ flex-panel-gallery/script.js
@@ -1,28 +1,50 @@
 const panels = document.querySelectorAll('.panel')
 
-var toggleOpen = function () {
-    if (!this.classList.contains('open')) {
-        panels.forEach(panel => {
-            panel.classList.remove('open')
-            panel.classList.remove('open-active')
-        })
-        this.classList.toggle('open')
-    } else {
-        panels.forEach(panel => {
-            panel.classList.remove('open')
-            panel.classList.remove('open-active')
-        })
-    }
+function toggleOpen() {
+    // console.log('Hello Alice');
+    this.classList.toggle('open');
 }
 
-var toggleActive = function (e) {
+function toggleActive(e) {
+    // console.log(e.propertyName);
     if (e.propertyName.includes('flex')) {
-        if(this.classList.contains('open')){
-            this.classList.toggle('open-active')
-        }
+        this.classList.toggle('open-active');
     }
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
-panels.forEach(panel => panel.addEventListener('transitioned', toggleActive))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+
+
+
+// const panels = document.querySelectorAll('.panel')
+
+// var toggleOpen = function () {
+//     if (!this.classList.contains('open')) {
+//         panels.forEach(panel => {
+//             panel.classList.remove('open')
+//             panel.classList.remove('open-active')
+//         })
+//         this.classList.toggle('open')
+//     } else {
+//         panels.forEach(panel => {
+//             panel.classList.remove('open')
+//             panel.classList.remove('open-active')
+//         })
+//     }
+// }
+
+// var toggleActive = function (e) {
+//     if (e.propertyName.includes('flex')) {
+//         if(this.classList.contains('open')){
+//             this.classList.toggle('open-active')
+//         }
+//     }
+// }
+
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+
+// panels.forEach(panel => panel.addEventListener('transitioned', toggleActive))
