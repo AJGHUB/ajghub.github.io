2f6f79156169ae3b212c5f0d50b49b3c943c51da
Alice Jowsey <12345alcatraz@gmail.com>
2020-10-26 20:03:10 +1300

    html js css modified

--- ajax_type_ahead/script.js
+++ ajax_type_ahead/script.js
@@ -1,15 +1,42 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = []
+const cities = [];
 
 window.fetch(endpoint)
-.then(response => response.json())
-.then(data => cities.push(...data))
+  .then(response => response.json())
+  .then(data => cities.push(...data))
 
 function findMatches (wordToMatch, cities) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi')
-        return place.city.match(regex) || place.state.match(regex)
-    })
-    
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi')
+    return place.city.match(regex) || place.state.match(regex)
+  })
 }
+
+function numberWithCommas (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+function displayMatches () {
+  const matchArr = findMatches(this.value, cities)
+  const regex = new RegExp(this.value, 'gi')
+
+  const html = matchArr.map(place => {
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `
+  }).join('')
+  suggestions.innerHTML = html
+}
+
+const searchInput = document.querySelector('.search')
+const suggestions = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
