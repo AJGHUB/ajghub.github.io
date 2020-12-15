401ab5132171db6a2ff197fdcd82bb102e6b6991
Alice Jowsey <12345alcatraz@gmail.com>
2020-12-15 17:59:16 +1300

    refactor functions

--- array_cardio_day2/script.js
+++ array_cardio_day2/script.js
@@ -51,7 +51,7 @@ const currentYear = (new Date()).getFullYear()
 //Some and every checks:
 //Array.prototype.some() //is at least one person 19 or older?
 
-//method 1 > 
+//method 1: 
 // const isAdult = people.some(function(person) {
 //     const currentYear = (newDate()).getFullYear();
 //     if(currentYear - person.year >= 19) {
@@ -60,27 +60,51 @@ const currentYear = (new Date()).getFullYear()
 // })
 
 
-//method uses implicit return of curly brackets - same as above, cleaner
+// method 2: uses arrow function with implicit return of curly brackets - same as above, cleaner
     var isAdult = people.some(p => currentYear - p.year >= 19)
     console.log({ isAdult })
-
+//____________________________________________________________________________________________
 
 //Array.prototype.every() //is everyone 19 or older?
-    var isAllAdults = people.every(p => currentYear - p.year >= 19)
+//method 1:
+// const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+// console.log(allAdults);  
+
+
+// method 2: uses arrow function with implicit return of curly brackets - same as above, cleaner 
+var isAllAdults = people.every(p => currentYear - p.year >= 19)
     console.log({ isAllAdults })
 
+//____________________________________________________________________________________________
 
 //Array.prototype.find()
-//Find is like filter, instead returns the one youre looking for
+//Find is like filter, instead returns the one you're looking for
 //Find the comment with the ID of 823423
+
+// method 1: const comment = comments.find(function(comment) {
+//    if(comment.id === 445566) {
+//        return true;
+//    }
+//});
+//console.log(comment)
+
+//method 2: uses arrow function with implicit return of curly brackets - same as above, cleaner
     const comment = comments.find(c => c.id === 334455)
     console.log(comment)
+//_____________________________________________________________________________________________
+
 
 //Array.prototype.findIndex()
 //Find the comment with this ID
 //Delete the comment with the ID of 334455
     const index = comments.findIndex(c => c.id === 334455)
-    comments.splice(index, 1)
-    console.table(comments)
+
+    // comments.splice(index, 1);
 
 
+//Or: build new array of the comments (used with redux)
+    const newComments = [
+        ...comments.slice(0, index),
+        ...comments.slice(index + 1) //spread operator used to spread comments into new array
+];
+console.table(newComments);
