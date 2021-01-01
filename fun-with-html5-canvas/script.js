c2c787600588c85b89e48f29ebe548e2e00688c5
Alice Jowsey <12345alcatraz@gmail.com>
2021-01-02 12:52:04 +1300

    js script bug fix

--- fun-with-html5-canvas/script.js
+++ fun-with-html5-canvas/script.js
@@ -14,45 +14,43 @@ let lastX = 0
 let lastY = 0
 let direction = true
 const color = {
-    hue: 0,
-    saturation: 100,
-    lightness: 50
+  hue: 0,
+  saturation: 100,
+  lightness: 50
 }
 
-function draw(e) {
-    if (!isDrawing) return
+function draw (e) {
+  if (!isDrawing) return
 
+  ctx.strokeStyle = `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`
+  ctx.beginPath()
+  ctx.moveTo(lastX, lastY)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY]
 
-    ctx.strokeStyle = `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`
-    ctx.beginPath()
-    ctx.moveTo(lastX, lastY)
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY]
+  color.hue++
+  if (color.hue >= 360) {
+    color.hue = 0
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction
+  }
 
-    color.hue++
-    if (color.hue >= 360) {
-        color.hue = 0
-    }
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-        direction = !direction
-    }
+  color.saturation = Math.min((e.x / window.innerWidth * 100) + 20, 100)
+  color.lightness = e.y / window.innerHeight * 40 - 20 + 50
 
-    color.saturation = Math.min((e.x / window.innerWidth * 100) + 20, 100)
-    color.lightness = e.y / window.innerHeight * 40 - 20 + 50
-
-    if (direction) {
-        ctx.lineWidth++
-    } else {
-        ctx.lineWidth--
-    }
+  if (direction) {
+    ctx.lineWidth++
+  } else {
+    ctx.lineWidth--
+  }
 }
 
 canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY]
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]
 })
-
 
 canvas.addEventListener('mousemove', draw)
 canvas.addEventListener('mouseup', () => (isDrawing = false))
