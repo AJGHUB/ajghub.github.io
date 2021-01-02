c3ca4c6861c790f5899d8aa25592afa294ac763b
Alice Jowsey <12345alcatraz@gmail.com>
2021-01-02 20:05:09 +1300

    get the elements

---
+++ custom-video-player/script.js
@@ -0,0 +1,21 @@
+/*___________________________Get Elements_______________________*/
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = progress.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+
+
+/*_________________________Build out functions___________________*/
+
+
+
+
+
+
+/*_________________________Hook up the event listeners___________*/
