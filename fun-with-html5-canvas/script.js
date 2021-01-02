5c508db817c38077d3a1ebf8cfd42044152f5158
Alice Jowsey <12345alcatraz@gmail.com>
2021-01-02 13:39:56 +1300

    notes

--- fun-with-html5-canvas/script.js
+++ fun-with-html5-canvas/script.js
@@ -7,12 +7,13 @@ ctx.strokeStyle = '#BADA55'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
 ctx.lineWidth = 36
-ctx.globalCompositeOperation = 'overlay'
+ctx.globalCompositeOperation = 'overlay' 
+ //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
 
 let isDrawing = false
-let lastX = 0
-let lastY = 0
-let direction = true
+let lastX = 0 //start the line
+let lastY = 0 //end the line
+let direction = true  //building up in line size 
 const color = {
   hue: 0,
   saturation: 100,
@@ -20,21 +21,23 @@ const color = {
 }
 
 function draw (e) {
-  if (!isDrawing) return
+  if (!isDrawing) return //stop the function from running when not moused down
 
-  ctx.strokeStyle = `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`
+  ctx.strokeStyle = `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)` //https://mothereffinghsl.com/
   ctx.beginPath()
+  //start from
   ctx.moveTo(lastX, lastY)
+  //go to
   ctx.lineTo(e.offsetX, e.offsetY)
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+  [lastX, lastY] = [e.offsetX, e.offsetY] //destructured array
 
   color.hue++
   if (color.hue >= 360) {
     color.hue = 0
   }
   if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction
+    direction = !direction 
   }
 
   color.saturation = Math.min((e.x / window.innerWidth * 100) + 20, 100)
@@ -44,7 +47,7 @@ function draw (e) {
     ctx.lineWidth++
   } else {
     ctx.lineWidth--
-  }
+  }  //increment linewidth then decreases linewidth 0>100 >99>0
 }
 
 canvas.addEventListener('mousedown', (e) => {
